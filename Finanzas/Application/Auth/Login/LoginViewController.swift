318005b039c89483e8f77977773028b5dffb42f9
GCoradini <gonzalocoradini@gmail.com>
2020-08-09 16:42:31 -0300

    refactor navigation bar style

--- Finanzas/Application/Auth/Login/LoginViewController.swift
+++ Finanzas/Application/Auth/Login/LoginViewController.swift
@@ -18,13 +18,7 @@ final class LoginViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Login"
-        self.navigationController!.navigationBar.barStyle = .black
-        self.navigationController!.navigationBar.isTranslucent = true
-        self.navigationController!.navigationBar.titleTextAttributes = [
-            .foregroundColor: UIColor.white,
-            .font: UIFont(name: "Arial Rounded MT Bold", size: 17)!
-        ]
-        self.navigationController!.navigationBar.tintColor = UIColor.orange
+        self.navigationController?.setStyleNavigationBar()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -33,29 +27,31 @@ final class LoginViewController: BaseViewController {
     }
     
     @IBAction func btnSignUp() {
+        print("Se presiono el boton de crear cuenta") //borrar luego
         self.navigationController?.pushViewController(SignUpViewController(), animated: true)
-        print("Registrarse presionado")
     }
     
     @IBAction func btnLogin() {
-        guard login.signIn(with: usernameTxtField.text,
-                           and: passwordTxtField.text
-        ) else {
-            print("Usuario o contraseña incorrecta")
-            return
+        //PA MOSTRAR POR CONSOLA borrar luego
+        print("Se ingreso:")
+        if let username = usernameTxtField.text {
+            print("username: \(username)")
+        }
+        if let password = passwordTxtField.text {
+            print("password: \(password)\n")
+        }
+        //-------------------------------------
+        
+        guard login.signIn(
+            with: usernameTxtField.text,
+            and: passwordTxtField.text
+            ) else {
+                print("No se pudo iniciar sesión")
+                return
         }
         self.navigationController?.pushViewController(HomeViewController(), animated: true)
-        
-        //PA MOSTRAR POR CONSOLA
-        print("Login presionado")
-        if let username = usernameTxtField.text {
-            print("Username: \(username)")
-        }
-        
-        if let password = passwordTxtField.text {
-            print(password)
-        }
     }
+    
     /*
     ///Borrar esto
     @IBAction func borrarDatos() {
---
+++ Finanzas/Application/Auth/StyleNavigationBar.swift
@@ -0,0 +1,21 @@
+//
+//  StyleNavigationBar.swift
+//  Finanzas
+//
+//  Created by Usuario on 09/08/2020.
+//  Copyright © 2020 Infinixsoft. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    func setStyleNavigationBar() {
+        self.navigationBar.barStyle = .black
+        self.navigationBar.isTranslucent = true
+        self.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont(name: "Arial Rounded MT Bold", size: 17)!
+        ]
+        self.navigationBar.tintColor = UIColor.orange
+    }
+}
